08e1adf8e6768ee8f0987bc94d1d48572388ee9d
Wahid Jamaalii As Shidqi <wahid@crowde.co>
2024-05-22 16:04:36 +0700

    Jawaban Question 1

---
+++ Soal1.js
@@ -0,0 +1,19 @@
+const Question1 = (x) => {
+    for (let i = 1; i <= x; i++){
+        let data = "";
+
+        if(i % 3 === 0 && i % 15 !== 0){
+            data += "fish";
+        }else if (i % 5 === 0 && i % 15 !== 0){
+            data += "bash";
+        }else if(i % 15 === 0){
+            data += "fish bash";
+        }else{
+            data += i;
+        }
+
+        console.log(data)
+    }
+}
+
+Question1(15)
